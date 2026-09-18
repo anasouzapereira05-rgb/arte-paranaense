f39b7ebb856ce52155f91443185f3251435acb98
anasouzapereira05-rgb <ana.souza.pereira05@escola.pr.gov.br>
2026-09-18 08:45:40 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const botoesCurtir = document.querySelectorA11(".curtir");
+botoesCurtir .forEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCuritr . addEverListener("click",curtir);
+function curtir(){
+    const contador = botaoCurtir .querySelector("span");
+    if(curtir === false){
+        contador.textContent++;
+        curtiu = true; }
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+} };   
